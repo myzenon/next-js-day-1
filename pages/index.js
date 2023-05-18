4ab892f56b4baedbd3dbea8397dcc0598b493faf
Boss <boss@odds.team>
2023-05-18 19:56:06 +0700

    Update

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,28 @@ import { useState } from 'react'
 
 export default function IndexPage() {
   const [inputValue, setInputValue] = useState('')
+  const [list, setList] = useState([])
+
+  const loadList = () => {
+    fetch('https://api.zenon.si/post')
+      .then(response => response.json())
+      .then(data => setList(data))
+  }
+
+  const tweet = () => {
+    setInputValue('')
+    fetch('https://api.zenon.si/post', {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ name: 'อะไรก็ได้', content: inputValue, }),
+    })
+      .then(() => loadList())
+  }
+
   return (
-    <div className="h-full bg-gray-100 flex items-start justify-center">
+    <div className="min-h-full bg-gray-100 flex flex-col items-center">
       <div className="w-1/2 flex flex-col items-end">
         <div className="w-full mt-32 bg-white p-6 rounded-lg shadow">
             <textarea
@@ -16,9 +36,30 @@ export default function IndexPage() {
               }}
             />
         </div>
-        <button className="mt-6 bg-gray-800 text-white font-bold px-8 py-4 rounded-lg shadow-lg">
-          Tweet {inputValue}
+        <button
+          className="mt-6 bg-gray-800 text-white font-bold px-8 py-4 rounded-lg shadow-lg"
+          onClick={tweet}
+        >
+          Tweet
         </button>
+        <button
+          className="bg-gray-400 text-white p-4 rounded-lg"
+          onClick={loadList}
+        >
+          Refresh
+        </button>
+      </div>
+      <div className="w-1/2 mt-8">
+        { list.map((data) => {
+          return (
+            <div key={data.id} className="mt-4 bg-white rounded-lg shadow-lg p-6">
+              <h1 className="text-xl font-bold">{data.name}</h1>
+              <div className="mt-2 text-gray-600">
+                {data.content}
+              </div>
+            </div>
+          )
+        }) }
       </div>
     </div>
   )
